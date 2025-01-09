e60a785d725d32f55e84185095f78977ac522910
huycuti22 <139739364+huycuti22@users.noreply.github.com>
2025-01-09 20:09:00 +0700

    090125

--- Key.lua
+++ Key.lua
@@ -1,26 +1,26 @@
-
 local success, key = pcall(function()
     return game:HttpGet('https://pastebin.com/raw/fMtF0rta')
 end)
 
 if success then
     print("안녕하세요 - Xin Chao - Hello")
+    if getgenv().Key == key then
+        print("OK")
+    else
+        print("Wrong")
+        local player = game.Players.LocalPlayer
+
+        local reason = "Wrong Key"
+
+        local message = string.format("You were kicked from this experience: %s", reason)
+        player:Kick(message)
+    end
 else
     warn("Failed to load key")
-    local player = game.Players. LocalPlayer
-
-
-    local reason = " Wrong Key"
-
-    local message = string.format("You were kicked from this experience"..reason)
-    player:Kick(message)
+    
 end
 
-if getgenv().Key == key then
-    print("OK")
-else
-    print("Wrong")
-end
+
 
 -- Library + Variables
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Loco-CTO/UI-Library/main/VisionLibV2/source.lua'))()
@@ -38,12 +38,12 @@ local function SendNotification(Name, Content, Image, Time)
         Name = Name,
         Text = Content,
         Icon = Image,
-        Duration = 3
+        Duration = Time or 3
     })
 end
 
 local function UpdateFile()
-    if isfile and writefile and isfile("SigmaHubConfig.txt") then
+    if isfile and writefile then
         writefile("SigmaHubConfig.txt", HttpService:JSONEncode(Settings))
     end
 end
@@ -57,9 +57,11 @@ local function LoadScript()
     end
 
     local GetScript
-    if game.PlaceId == 2753915549 then
-        GetScript = Request({Url = "https://raw.githubusercontent.com/huycuti22/autochestBF/refs/heads/main/AutoChest.lua", Method = "GET"})
-    end
+    pcall(function()
+        if game.PlaceId == 2753915549 then
+            GetScript = Request({Url = "https://raw.githubusercontent.com/huycuti22/autochestBF/refs/heads/main/AutoChest.lua", Method = "GET"})
+        end
+    end)
 
     if GetScript and GetScript.Body and GetScript.Body ~= "404: Not Found" then
         task.wait(math.random(1, 4))
@@ -74,8 +76,7 @@ end
 SendNotification("Sigma Hub", "Loading... Please wait.", "rbxassetid://13328029686", 5)
 
 -- UI Library Setup
-local UILibrary = loadstring(game:HttpGet('https://raw.githubusercontent.com/Loco-CTO/UI-Library/main/VisionLibV2/source.lua'))()
-local Window = UILibrary:Create({
+local Window = Library:Create({
     Name = "Sigma Hub - Loader",
     Footer = "By Ghuy",
     ToggleKey = Enum.KeyCode.RightShift,
@@ -93,9 +94,8 @@ local Section1 = Tab:Section({
     Name = "Main"
 })
 
--- Buttons and Toggles
 Section1:Button({
-    Name = "Load Script",
+    Name = "Load Script - " .. GameName,
     Callback = function()
         LoadScript()
     end
@@ -115,7 +115,7 @@ end
 -- Config Handling
 if isfile and not isfile("SigmaHubConfig.txt") and writefile then
     writefile("SigmaHubConfig.txt", HttpService:JSONEncode(Settings))
-elseif isfile and isfile("SigmaHubConfig.txt") then
+elseif isfile and readfile("SigmaHubConfig.txt") then
     Settings = HttpService:JSONDecode(readfile("SigmaHubConfig.txt"))
 end
 
