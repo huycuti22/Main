9d134e9ff86260917bf7e7dd7e77f1396f1d4b84
Ghuy <139739364+huycuti22@users.noreply.github.com>
2025-03-20 20:21:24 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@
 local Config = loadstring(game:HttpGet("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/Config.lua"))()
 local Utilities = loadstring(game:HttpGet("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/Utilities.lua"))()
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/ESP.lua"))()(Config, Utilities)
-local MiddleClick = loadstring("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/MiddleClick.lua")()
+local MiddleClick = loadstring(game:HttpGet("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/MiddleClick.lua"))()
 local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/Aimbot.lua"))()
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/huycuti22/Main/refs/heads/main/UI.lua"))()(Config, ESP, MiddleClick, Aimbot)
 
